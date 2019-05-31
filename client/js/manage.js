8ba8ea5c00ff9f7f9a81d1eb784489d48077fcd1
Ryan Sjostrand <ryansjostrand@gmail.com>
2019-05-30 19:45:33 -0500

    Inital student add request support.

---
+++ client/js/manage.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+window.onload = function() {
+    console.info("Hello")
+
+    $("#studentAddSaveButton").click(function() {
+        console.info("Button: Student Add Save")
+
+        // TODO Add some sort of input validation
+
+        var data = {}
+        data["firstName"] = $("#studentAddFirstNameInput").val()
+        data["lastName"] = $("#studentAddLastNameInput").val()
+        data["graduationYear"] = $("#studentAddGradutationYearInput").val()
+        data["id"] = $("#studentAddIdInput").val()
+        data["schoolId"] = $("#studentAddSchoolIdInput").val()
+
+        var request = JSON.stringify(data)
+
+        console.info("Request: " + request)
+        
+        var url = "/api/students"
+        var postReq = new XMLHttpRequest(); 
+        postReq.open("POST", url)
+        postReq.setRequestHeader('Content-Type', 'application/json');
+        postReq.send(request);
+
+        postReq.onreadystatechange = function() {
+            console.log("POST Readystate: " + postReq.readyState)
+            if(postReq.readyState == 4) {
+                if (postReq.status == 200) {
+                    // Success
+                    console.info("Requst succedded")
+
+                } else {
+                    // Failed
+                    console.error("Request failed")
+                }
+            }
+        }
+
+    })
+}
